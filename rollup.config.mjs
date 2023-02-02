02de3b6b9da01fdc8455be18b6be9f2642703c2e
Brad Thomason <brad@badfeather.com>
2023-02-02 15:31:40 -0500

    more deployment stuff

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
 import terser from '@rollup/plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+
 let files = [
 	'scripts.js',
 ];
