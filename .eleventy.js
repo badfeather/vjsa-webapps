6c6beb7edf5793af18bd0fc8a54620014a60b8de
Brad Thomason <brad@badfeather.com>
2023-02-04 11:17:01 -0500

    more url shenanigans

--- .eleventy.js
+++ .eleventy.js
@@ -13,7 +13,7 @@ module.exports = function (eleventyConfig) {
 		dir: {
 			input: "src",
 			output: "dist",
-		}
-		// , pathPrefix: "/vjsa-webapps/"
+		}, 
+		pathPrefix: "/vjsa-webapps/"
 	};
 };
