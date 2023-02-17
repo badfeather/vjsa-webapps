e0b9341f58854afeed981c2440f76ad5cb8812d0
Brad Thomason <brad@badfeather.com>
2023-02-17 17:54:02 -0500

    changed project key

--- dist/w3p1/js/components/cart.js
+++ dist/w3p1/js/components/cart.js
@@ -1,6 +1,6 @@
 import {store, component} from '../../../js/vendor/reef.es.js';
 
-let storageKey = 'w2p3CartItems';
+let storageKey = 'w3p1CartItems';
 
 /**
  * Get saved cart data from session storage
