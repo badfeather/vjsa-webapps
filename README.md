ab95b27bb9d78b30dcabba3bd6a8b9192ec15726
Brad Thomason <brad@badfeather.com>
2023-02-22 08:29:55 -0500

    changed storage key

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Vanilla JS Academy - Web Apps
-Version: 1.0.72
+Version: 1.0.75
---
+++ dist/w4p1/js/components/endpoints.js
@@ -0,0 +1,8 @@
+// The photos API endpoint
+let photosURL = 'https://worker-photos.bradthomason.workers.dev';
+let photosDBURL = 'https://worker-photos-db.bradthomason.workers.dev';
+
+// The stripe API endpoint
+let stripeURL = 'https://worker-stripe.bradthomason.workers.dev';
+
+export {photosURL, photosDBURL, stripeURL};
