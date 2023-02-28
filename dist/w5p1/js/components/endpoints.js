bc390c85e75dec87ba230d44b53b1e182bf1aec5
Brad Thomason <brad@badfeather.com>
2023-02-28 11:14:59 -0500

    updated w5p1 with async instead of fetch

--- dist/w5p1/js/components/endpoints.js
+++ dist/w5p1/js/components/endpoints.js
@@ -4,5 +4,6 @@ let photosDBURL = 'https://worker-photos-db.bradthomason.workers.dev';
 
 // The stripe API endpoint
 let stripeURL = 'https://worker-stripe.bradthomason.workers.dev';
+let dummyAuthURL = 'https://vanillajsacademy.com/api/login.php';
 
-export {photosURL, photosDBURL, stripeURL};
+export {photosURL, photosDBURL, stripeURL, dummyAuthURL};
--- src/w5p1/js/page-login.js
+++ src/w5p1/js/page-login.js
@@ -1,3 +1,6 @@
+
+import {dummyAuthURL} from './components/endpoints.js';
+
 function serialize (data) {
 	let obj = {};
 	for (let [key, value] of data) {
@@ -13,36 +16,39 @@ function serialize (data) {
 	return obj;
 }
 
+async function submitHandler (event) {
+	event.preventDefault();
+	if (form.hasAttribute('data-submitting')) return;
+	let {username, password} = serialize(new FormData(form));
+	if (!username || !password) {
+		status.textContent = 'Please include a username and password.';
+		return;
+	}
+
+	form.setAttribute('data-submitting', true);
+	let auth = btoa(`${username}:${password}`);
+
+	try {
+		let login = await fetch(dummyAuthURL, {
+			method: 'POST',
+			headers: {
+				'Authorization': `Basic ${auth}`
+			}
+		});
+
+		if (!login.ok) throw login;
+		let {token} = await login.json();
+		status.innerText = `Success! Your authentication token is: ${token}`;
+
+	} catch (error) {
+		form.reset();
+		form.removeAttribute('data-submitting');
+		status.innerText = 'Something went wrong. Please try again.';
+	}
+}
+
 let form = document.querySelector('[data-form]');
 let status = document.querySelector('[data-form-status]');
 
-if (form && status) form.addEventListener('submit', function(event) {
-	event.preventDefault();
-	let formData = new FormData(form);
-	let dataObj = serialize(formData);
-	console.log(dataObj);
-	let username = dataObj.username;
-	let password = dataObj.password;
-	let auth = btoa(`${username}:${password}`);
-
-	fetch('https://vanillajsacademy.com/api/login.php', {
-		headers: {
-			'Authorization': `Basic ${auth}`
-		}
-
-	}).then(function (response) {
-		if (response.ok) {
-			return response.json();
-		}
-		throw response;
-
-	}).then(function (data) {
-		status.innerHTML = 'Hooray! That worked.';
-		console.log(data);
-
-	}).catch(function (error) {
-		status.innerHTML = 'Sorry, the username or password you entered is invalid.';
-		console.warn(error);
-	});
-});
+if (form && status) form.addEventListener('submit', submitHandler);
 
