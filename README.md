204541cba19fcf506e69542dc33ecf102aef92b9
Brad Thomason <brad@badfeather.com>
2023-03-03 13:01:02 -0500

    fixed w5p3

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Vanilla JS Academy - Web Apps
-Version: 1.0.102
+Version: 1.0.103
--- src/w5p3/js/components/admin.js
+++ src/w5p3/js/components/admin.js
@@ -1,11 +1,13 @@
 import {dashboardURL} from './endpoints.js';
 // import {savePhotos} from './photos.js';
+import {getToken} from './token.js';
 
 async function fetchAuthPhotos (endpoint = dashboardURL) {
 	if (!endpoint) return;
+	let token = getToken();
 
 	try {
-		let response = await fetch(endpoint);
+		let response = await fetch(`${endpoint}?token=${token}`);
 		if (!response.ok) throw response;
 		let photos = await response.json();
 		// savePhotos(photos);
@@ -13,7 +15,7 @@ async function fetchAuthPhotos (endpoint = dashboardURL) {
 
 	} catch (error) {
 		console.warn(error);
-		return false;
+		return [];
 	}
 }
 
